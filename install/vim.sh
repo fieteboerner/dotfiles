800ccf3531fceae131d8106ed51d3d7701bab22f
Fiete Börner <fieteboerner@googlemail.com>
2016-02-21 22:26:33 +0100

    add snippet function to setup

--- install/vim.sh
+++ install/vim.sh
@@ -12,7 +12,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git
 echo "Installing vundle plugins"
 vim +PluginInstall +qall
 
-
+echo "Setting up color scheme"
 mkdir ~/.vim/colors
 cd ~/.vim/colors
 wget https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim
+
+echo "Installing snippets"
+ln -s $DOTFILES/vim/snippets.symlink ~/.vim/snippets
