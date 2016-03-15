5ca7253132c8126c2dd48252bb4887f07b576ab3
Fiete Börner <fieteboerner@googlemail.com>
2016-03-15 21:58:46 +0100

    install german dictionary; change tmux time format
    add key mappings for changing between german and english dictionary
    expandtab (no tabs) as default in vim

--- install/vim.sh
+++ install/vim.sh
@@ -17,5 +17,11 @@ mkdir ~/.vim/colors
 cd ~/.vim/colors
 wget https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim
 
+echo "Download Dictionaries"
+mkdir ~/.vim/spell
+cd ~/.vim/spell
+wget http://ftp.vim.org/pub/vim/runtime/spell/en.utf-8.spl
+wget http://ftp.vim.org/pub/vim/runtime/spell/de.utf-8.spl
+
 echo "Installing snippets"
 ln -s $DOTFILES/vim/snippets.symlink ~/.vim/snippets
