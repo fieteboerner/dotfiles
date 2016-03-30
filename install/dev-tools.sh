3b05fbd77cc4ea3f20f4e9e9b181e2f4d41583e2
Fiete Börner <fieteboerner@googlemail.com>
2016-03-30 22:42:53 +0200

    add PHP support to vim
    add several PHP plugins to vim
        better syntax highlighting
        code linter, mess detector
        debugger
        reformatter
    
    update install/dev-tools.sh for installing php-cs-fixer
    add several html snippets
    update oh-my-zsh

--- install/dev-tools.sh
+++ install/dev-tools.sh
@@ -6,6 +6,9 @@ sudo npm install -g bower gulp
 sudo gem install sass
 # composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+# php-cs-fixer
+sudo wget http://get.sensiolabs.org/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
+sudo chmod a+x /usr/local/bin/php-cs-fixer
 # configure apache
 # mysql path ummappen
 
