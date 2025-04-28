dd59f262ae21b79fe1a98ba8bce04121fd39ecda
Fiete Börner <fboerner@adzlocal.de>
2025-04-28 17:07:26 +0200

    create php class snippet

--- config/nvim/lua/flitzfiete/plugins/luasnip.lua
+++ config/nvim/lua/flitzfiete/plugins/luasnip.lua
@@ -1,4 +1,7 @@
 local ls = require("luasnip")
+local t = ls.text_node
+local f = ls.function_node
+local i = ls.insert_node
 
 local M = {}
 
@@ -30,12 +33,36 @@ M.setup = function()
 
     -- PHP
     ls.add_snippets("php", {
-        ls.parser.parse_snippet("class", "class $1\n{\n    $0\n}"),
         ls.parser.parse_snippet("pubf", "public function $1($2): $3\n{\n    $0\n}"),
         ls.parser.parse_snippet("prif", "private function $1($2): $3\n{\n    $0\n}"),
         ls.parser.parse_snippet("prof", "protected function $1($2): $3\n{\n    $0\n}"),
         ls.parser.parse_snippet("testt", "public function test_$1()\n{\n    $0\n}"),
         ls.parser.parse_snippet("testa", "/** @test */\npublic function $1()\n{\n    $0\n}"),
+        ls.snippet("class", {
+            t({ "<?php", "", "namespace " }),
+            f(function(_, snip)
+                local filename = snip.env.TM_FILENAME
+                local filepath = snip.env.TM_DIRECTORY
+                local base_namespace = "App"
+
+                -- Extract relative path from "app" directory
+                local relative_path = filepath:match("app/(.*)")
+                if relative_path then
+                    relative_path = relative_path:gsub("/", "\\") -- Convert to PHP namespace format
+                    return { base_namespace .. "\\" .. relative_path }
+                else
+                    return { base_namespace }
+                end
+            end, {}),
+            t({ ";", "", "/** */", "class " }),
+            f(function(_, snip)
+                local filename = snip.env.TM_FILENAME
+                return { filename and filename:gsub("%.php$", "") or "ClassName" }
+            end, {}),
+            t({ "", "{", "" }),
+            i(0),
+            t({ "", "}" }),
+        }),
     })
 
     -- takes care to not jump around in the document by tabbing
